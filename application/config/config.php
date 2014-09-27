24f8f6268b30be4bac39f50b6f45f2bcb6328e99
Ted Kesgar <t.kesgar@live.com>
2014-09-27 17:31:55 +0700

    Fixed index page

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ $config['base_url']	= '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'front/info.html';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
